050813ed065375561f36572d91f0e5a8475412d6
Brandon <sugb73@gmail.com>
2021-02-11 11:12:18 -0500

    commit tree c9ae0720820cc36da2b94602229c2509e691c496

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
 # Add your code here
+class Dog
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end    
+    
+    def name
+        @name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each do |pr_all|
+            puts pr_all.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+
+
+
+
+
+    def self.clear_all
+        @@all = []
+    end
+
+end
